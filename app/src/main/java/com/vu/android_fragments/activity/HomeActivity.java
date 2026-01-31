2297733bdef19c850c6cff7f18e766d8a1a309a9
Vikash Udarsha <udarshav@gmail.com>
2026-01-31 08:46:48 +0530

    first commit

--- app/src/main/java/com/vu/android_fragments/activity/HomeActivity.java
+++ app/src/main/java/com/vu/android_fragments/activity/HomeActivity.java
@@ -4,12 +4,8 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.vu.android_fragments.R;
-import com.vu.android_fragments.fragment.settingFragment;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -19,11 +15,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-          if (savedInstanceState == null){
-              getSupportFragmentManager().beginTransaction()
-                      .setReorderingAllowed(true)
-                      .add(R.id.fragment_container_view, settingFragment.class, null)
-                      .commit();
-          }
+
     }
 }
--- app/src/main/java/com/vu/android_fragments/fragment/HomeFragment.java
+++ app/src/main/java/com/vu/android_fragments/fragment/HomeFragment.java
@@ -11,17 +11,10 @@ import androidx.fragment.app.Fragment;
 
 import com.vu.android_fragments.R;
 
-public class settingFragment extends Fragment {
-
-//    public settingFragment(){
-//        super(R.layout.fragment_setting);
-//
-//    }
-
-
+public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-              return inflater.inflate(R.layout.fragment_setting, container, false);
+        return  inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
